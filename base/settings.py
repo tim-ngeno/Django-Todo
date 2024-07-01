5cdff6432f52ff25064b9b2f494643c36ae0890e
Tim Ngeno <timngeno424@gmail.com>
2024-07-01 16:47:28 +0300

    Include STATICFILES_DIR to load static files

--- base/settings.py
+++ base/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "tasks.apps.TasksConfig",
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = "base.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,12 +118,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Where to redirect a user when they try to access unauthenticated routes
+LOGIN_URL = "/login"
 
-LOGIN_REDIRECT_URL = '/tasks'
-LOGOUT_REDIRECT_URL = '/login'
+# Redirects after authentication and for logging out
+LOGIN_REDIRECT_URL = "/tasks"
+LOGOUT_REDIRECT_URL = "/login"
